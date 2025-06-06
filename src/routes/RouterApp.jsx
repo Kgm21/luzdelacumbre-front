52ddc5b780aa44632ea38d5e4d661106e1c95378
Kgm21 <karenmanchado19@gmail.com>
2025-06-05 23:29:21 -0300

    nuievos cambios

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import { NavigateApp } from '../components/NavigateApp' 
-import { FooterComponent } from '../components/FooterComponent'
-import { Route, Routes } from 'react-router-dom'
-import { AboutUs, Error404, HomePage, LoginPage } from '../pages'
+import React from 'react';
+import { NavigateApp } from '../components/NavigateApp';
+import { FooterComponent } from '../components/footerComponent';
+import { Route, Routes } from 'react-router-dom';
+import { AboutUs, Error404, HomePage, LoginPage } from '../pages';
+import { Contactos } from '../pages/contactos';
 
 export const RouterApp = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
+      {/* Navbar */}
       <NavigateApp />
 
-      <Routes>
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/about' element={<AboutUs />} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
+      {/* Contenido principal */}
+      <main className="flex-fill">
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Error404 />} />
+          <Route path='/contactos' element={<Contactos/>}></Route>
+        </Routes>
+      </main>
 
+      {/* Footer */}
       <FooterComponent />
     </div>
-  )
-}
+  );
+};
