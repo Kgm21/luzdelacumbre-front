67e192ae07ca04834ef51faddf189c7e00806bb9
Nanci-Guzman <guz.zulema23@gmail.com>
2025-06-30 20:48:01 -0300

    Se creó la página del Error404

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import NavigateApp from "./components/navbar/NavigateApp";
 import FooterComponent from "./components/footer/FooterComponent";
 import HomePage from "./pages/homepage";
 import AboutUs from "./pages/aboutus";
 import LoginPage from "./pages/login";
-import Error404 from "./pages/error404";
+import Error404 from "./pages/Error404";
 import Contactos from "./pages/contactos";
 import AdminPage from "./pages/adminPage";
 import Registro from "./pages/registro";
@@ -14,9 +14,8 @@ import { AuthProvider } from "./context/AuthContext";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Cabanias  from './pages/cabanias';
+import Cabanias from "./pages/cabanias";
 import Reservas from "./pages/reservas";
-
 
 function App() {
   const [usuarioAutenticado, setUsuarioAutenticado] = useState(false);
@@ -25,18 +24,26 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <div className="d-flex flex-column min-vh-100">
-          <NavigateApp usuarioAutenticado={usuarioAutenticado} setUsuarioAutenticado={setUsuarioAutenticado} />
+          <NavigateApp
+            usuarioAutenticado={usuarioAutenticado}
+            setUsuarioAutenticado={setUsuarioAutenticado}
+          />
           <main className="flex-fill">
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/about" element={<AboutUs />} />
-              <Route path="/login" element={<LoginPage setUsuarioAutenticado={setUsuarioAutenticado} />} />
+              <Route
+                path="/login"
+                element={
+                  <LoginPage setUsuarioAutenticado={setUsuarioAutenticado} />
+                }
+              />
               <Route path="/registro" element={<Registro />} />
               <Route path="/contactos" element={<Contactos />} />
               <Route path="/reservas" element={<Reservas />} />
-              <Route path='/cabanias' element={<Cabanias/>}></Route>
-            
-            <Route
+              <Route path="/cabanias" element={<Cabanias />}></Route>
+
+              <Route
                 path="/administracion"
                 element={
                   <ProtectedRoute>
@@ -44,7 +51,8 @@ function App() {
                   </ProtectedRoute>
                 }
               />
-              <Route path="*" element={<Error404 />} />
+              <Route path="/error404" element={<Error404 />}></Route>
+              <Route path="*" element={<Navigate to="/error404" />} />
             </Routes>
           </main>
           <FooterComponent />
--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import "./Error404.css";
+import "./styles/Error404.css";
 
 const Error404 = () => {
   return (
     <div className="error-container">
       <img
-        src="/imagenes/cabana-vieja.png" // poné una imagen relativa en public/
-        alt="Cabaña antigua"
+        src="/public/personajes/error.jpg"
+        alt="imagen cabaña"
         className="error-img"
       />
-      <h1>404 - Página no encontrada</h1>
+
       <p>La cabaña que estás buscando no existe.</p>
     </div>
   );
